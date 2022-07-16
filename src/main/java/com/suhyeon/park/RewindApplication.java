9c530959ebcfdb879f5f3d0206e44ad7f6ecba1c
suhyeonP <ahah12k@gmail.com>
2022-07-16 23:34:55 +0900

    :tada:

---
+++ src/main/java/com/suhyeon/park/RewindApplication.java
@@ -0,0 +1,13 @@
+package com.suhyeon.park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suhyeon/park/RewindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhyeon.park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
